30fa8aad42457515ef4931496a5b9609504b0edf
therahulmehta <rahul95mehta@gmail.com>
2020-06-11 18:07:38 -0400

    custom render prop for dev

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,53 @@
-import React from 'react'
-
+import React, { useState } from "react";
+import Container from "react-bootstrap/Container";
+import Table from "react-bootstrap/Table";
+import LogItem from "./LogItem";
 const App = () => {
-	return (
-		<div className='app'>
-			<h1>React Electron Boilerplate</h1>
-			<p>This is a simple boilerplate for using React with Electron</p>
-		</div>
-	)
-}
+  // temp logs
+  const [logs, setLogs] = useState([
+    {
+      id: 1,
+      text: "This is log",
+      priority: "low",
+      user: "rahul",
+      create: new Date().toString(),
+    },
+    {
+      id: 2,
+      text: "This is log",
+      priority: "moderate",
+      user: "rahul",
+      create: new Date().toString(),
+    },
+    {
+      id: 3,
+      text: "This is log",
+      priority: "high",
+      user: "rahul",
+      create: new Date().toString(),
+    },
+  ]);
 
-export default App
+  return (
+    <Container>
+      <Table>
+        <thead>
+          <tr>
+            <th>Priority</th>
+            <th>Log Text</th>
+            <th>User</th>
+            <th>Created</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {logs.map((log) => (
+            <LogItem />
+          ))}
+        </tbody>
+      </Table>
+    </Container>
+  );
+};
+
+export default App;
