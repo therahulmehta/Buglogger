0aea4d638aa92a29f120276c47554b111bd97b73
therahulmehta <rahul95mehta@gmail.com>
2020-06-11 22:35:27 -0400

    validation and alerts added

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Container from "react-bootstrap/Container";
 import Table from "react-bootstrap/Table";
+import Alert from "react-bootstrap/Alert";
 import LogItem from "./LogItem";
 import AddLogItem from "./AddLogItem";
 
@@ -30,15 +31,44 @@ const App = () => {
     },
   ]);
 
+  const [alert, setAlert] = useState({
+    show: false,
+    message: "",
+    variant: "success",
+  });
+
+  function showAlert(message, variant = "success", seconds = 3000) {
+    setAlert({
+      show: true,
+      message,
+      variant,
+    });
+
+    setTimeout(() => {
+      setAlert({
+        show: false,
+        message: "",
+        variant: "success",
+      });
+    }, seconds);
+  }
+
   function addItem(item) {
+    if (item.text === "" || item.user === "" || item.priority === "") {
+      showAlert("Please enter all fields", "danger");
+      return false;
+    }
     item._id = Math.floor(Math.random() * 90000) + 10000;
     item.created = new Date().toString();
     setLogs([...logs, item]);
+
+    showAlert("Log Added");
   }
 
   return (
     <Container>
       <AddLogItem addItem={addItem} />
+      {alert.show && <Alert variant={alert.variant}> {alert.message}</Alert>}
       <Table>
         <thead>
           <tr>
